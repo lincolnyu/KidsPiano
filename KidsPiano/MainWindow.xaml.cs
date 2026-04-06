4d5101dd6ce271630f16ed97237c41b8f54914f3
Lincoln Yu <lincoln.yu@hydroalgorithmics.com>
2026-04-06 15:08:54 +1000

    Initial version: UI layout.

---
+++ KidsPiano/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Win32;
+
+namespace KidsPiano;
+
+public partial class MainWindow : Window
+{
+    private double _currentSpeed = 1.0;
+    private bool _isPlaying;
+    private string _lastFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Loaded += MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        // Initialize WebView2
+        await webViewScore.EnsureCoreWebView2Async(null);
+        // TODO: Later we will load OpenSheetMusicDisplay here
+        webViewScore.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+    }
+
+    private void btnOpen_Click(object sender, RoutedEventArgs e)
+    {
+        var dlg = new OpenFileDialog
+        {
+            Filter = "MusicXML files (*.musicxml;*.xml)|*.musicxml;*.xml|MIDI files (*.mid)|*.mid|All files (*.*)|*.*",
+            InitialDirectory = _lastFolder
+        };
+
+        if (dlg.ShowDialog() == true)
+        {
+            _lastFolder = Path.GetDirectoryName(dlg.FileName);
+            txtSongName.Text = "Loading..."; // Will extract real title later
+            LoadMusicFile(dlg.FileName);
+        }
+    }
+
+    private void LoadMusicFile(string filePath)
+    {
+        // TODO: Step-by-step we will implement parsing + rendering here
+        MessageBox.Show($"File selected: {filePath}\n\nParsing and rendering will be implemented next!",
+            "Kids Piano 🎹", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    // Speed buttons
+    private void SpeedButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && double.TryParse(btn.Tag?.ToString(), out var speed)) _currentSpeed = speed;
+        // Highlight selected button later
+    }
+
+    private void btnPlayPause_Click(object sender, RoutedEventArgs e)
+    {
+        _isPlaying = !_isPlaying;
+        btnPlayPause.Content = _isPlaying ? "⏸ Pause" : "▶ Play";
+        // TODO: Start/stop playback or tracking
+    }
+
+    private void btnRestart_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO: Reset to beginning
+        MessageBox.Show("Restarting from the beginning! 🎵", "Kids Piano");
+    }
+
+    private void btnNext_Click(object sender, RoutedEventArgs e)
+    {
+        /* TODO */
+    }
+
+    private void btnPrev_Click(object sender, RoutedEventArgs e)
+    {
+        /* TODO */
+    }
+
+    private void CoreWebView2_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+    {
+        // Will handle JS → C# messages for note detection feedback later
+    }
+}
